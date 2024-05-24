9a3517ace75480a92fa6cb0e4b6cba3dd17e458d
GeorgiyIzmailov <georgiy-izmailov-v@inkeep.com>
2024-05-24 19:06:57 +0300

    add cron job

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { type InferSelectModel } from "drizzle-orm";
+import { CronJob } from 'cron';
 import { App, LogLevel } from "@slack/bolt";
 import { CombinedEvaluationTable } from "./drizzleClient/schema";
 import { initializationDrizzleClient } from "./drizzleClient/db";
@@ -19,16 +20,36 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
-  // logLevel: LogLevel.DEBUG,
+  logLevel: LogLevel.DEBUG,
 });
 
-(async () => {
+const main = async () => {
   try {
-    await initializationDrizzleClient()
+    const { client, start } = app;
+    await initializationDrizzleClient();
+    await start(process.env.PORT || 3000);
 
-    await app.start(process.env.PORT || 3000);
     console.log('⚡️ Bolt app is running! ⚡️');
+
+    const job = new CronJob(
+      '0 0 0 * * *', // cronTime (every day)
+      async function () {
+        const response = await client.chat.postMessage({
+          channel: process.env.NOTIFICATION_CHANNEL_ID!,
+          text: ""
+        });
+
+        console.log("response", response);
+      },
+      null,
+      true,
+      'America/Los_Angeles'
+    );
+
+    job.start();
   } catch (error) {
     console.error('Unable to start App', error);
   }
-})();
+}
+
+main();
