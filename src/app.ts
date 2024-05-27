a9871352ff886f0c93ea7c5f7da6bfe95ad06761
GeorgiyIzmailov <georgiy-izmailov-v@inkeep.com>
2024-05-27 18:54:23 +0300

    update schema + get data from db

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import { type InferSelectModel } from "drizzle-orm";
-import { CronJob } from 'cron';
+import { gte, and, lte } from "drizzle-orm";
 import { App, LogLevel } from "@slack/bolt";
-import { CombinedEvaluationTable } from "./drizzleClient/schema";
 import { initializationDrizzleClient } from "./drizzleClient/db";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
+import * as schema from './drizzleClient/schema';
 
 require('dotenv').config();
 
@@ -12,10 +13,6 @@ export type GetQuestionFromAnalysisTable = {
   endDate: Date;
 };
 
-export type QuestionWithLabels = InferSelectModel<
-  typeof CombinedEvaluationTable
->;
-
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   socketMode: true,
@@ -23,32 +20,63 @@ const app = new App({
   logLevel: LogLevel.DEBUG,
 });
 
+const drizzleClient = new Client({
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+});
+
+// 2024-05-15 18:46:55.423 +0300
+
+// const job = new CronJob(
+//   '0 0 0 * * *', // cronTime (every day)
+//   async function () {
+//     const response = await client.chat.postMessage({
+//       channel: process.env.NOTIFICATION_CHANNEL_ID!,
+//       text: ""
+//     });
+
+//     console.log("response", response);
+//   },
+//   null,
+//   true,
+//   'America/Los_Angeles'
+// );
+
+// job.start();
+
 const main = async () => {
   try {
-    const { client, start } = app;
-    await initializationDrizzleClient();
-    await start(process.env.PORT || 3000);
-
-    console.log('⚡️ Bolt app is running! ⚡️');
-
-    const job = new CronJob(
-      '0 0 0 * * *', // cronTime (every day)
-      async function () {
-        const response = await client.chat.postMessage({
-          channel: process.env.NOTIFICATION_CHANNEL_ID!,
-          text: ""
-        });
-
-        console.log("response", response);
-      },
-      null,
-      true,
-      'America/Los_Angeles'
+    const currentDate = new Date("2024-05-15 18:46:55.423 +0300");
+    const prevDay = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      currentDate.getDate() - 1,
     );
 
-    job.start();
+    const db = drizzle(drizzleClient, { schema, logger: true });
+    await initializationDrizzleClient(drizzleClient, db);
+
+    await app.start(process.env.PORT || 3000);
+    console.log('⚡️ Bolt app is running! ⚡️');
+
+    const repsonseQuestions = await db.select({
+      message_id: schema.messageDashboard.messageId,
+      created_at: schema.messageDashboard.createdAt,
+      chat_session_id: schema.messageDashboard.chatSessionId,
+      question: schema.messageDashboard.question,
+      thumbs_up: schema.messageDashboard.thumbsUp,
+      thumbs_down: schema.messageDashboard.thumbsDown,
+      is_feature_supported: schema.messageDashboard.isFeatureSupported,
+    }).from(schema.messageDashboard).where(and(gte(schema.messageDashboard.createdAt, prevDay), lte(schema.messageDashboard.createdAt, currentDate)))
+
+    console.log("repsonseQuestions", repsonseQuestions);
   } catch (error) {
     console.error('Unable to start App', error);
+  } finally {
+    await drizzleClient.end()
   }
 }
 
--- src/drizzleClient/db.ts
+++ src/drizzleClient/db.ts
@@ -1,27 +1,14 @@
-import { Client } from "pg";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { type Client } from "pg";
+import { NodePgDatabase } from "drizzle-orm/node-postgres";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
+import * as schema from "./schema"
 
-export const initializationDrizzleClient = async () => {
-    const drizzleClient = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: Number(process.env.POSTGRES_PORT),
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-    });
-    const db = drizzle(drizzleClient, { logger: true });
-
+export const initializationDrizzleClient = async (drizzleClient: Client, db: NodePgDatabase<typeof schema>) => {
     await drizzleClient.connect();
 
     await migrate(db, {
         migrationsFolder: "drizzle",
     })
-        .then(() => {
-            console.log("migrations complete")
-        })
+        .then(() => console.log("migrations complete"))
         .catch((e) => console.error(e))
-        .finally(async () => {
-            await drizzleClient.end()
-        });
 }
