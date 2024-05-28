4b2431fbadc79f1ee925f89765a14e288b25fefa
GeorgiyIzmailov <georgiy-izmailov-v@inkeep.com>
2024-05-28 16:05:15 +0300

    add template for message + cron job

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
 import { gte, and, lte } from "drizzle-orm";
 import { App, LogLevel } from "@slack/bolt";
 import { initializationDrizzleClient } from "./drizzleClient/db";
-import { drizzle } from "drizzle-orm/node-postgres";
+import { NodePgDatabase, drizzle } from "drizzle-orm/node-postgres";
+import { CronJob } from "cron";
 import { Client } from "pg";
 import * as schema from './drizzleClient/schema';
 
 require('dotenv').config();
 
-export type GetQuestionFromAnalysisTable = {
-  projectId: string;
-  startDate: Date;
-  endDate: Date;
-};
+export type GetQuestionFromMessageDashboardTable = {
+  message_id: string;
+  created_at: Date;
+  chat_session_id: string;
+  organization_id: string;
+  project_id: string;
+  question: string | null;
+  thumbs_up: boolean;
+  thumbs_down: boolean;
+  is_feature_supported: boolean;
+}[];
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -28,33 +35,74 @@ const drizzleClient = new Client({
   database: process.env.POSTGRES_DB,
 });
 
-// 2024-05-15 18:46:55.423 +0300
 
-// const job = new CronJob(
-//   '0 0 0 * * *', // cronTime (every day)
-//   async function () {
-//     const response = await client.chat.postMessage({
-//       channel: process.env.NOTIFICATION_CHANNEL_ID!,
-//       text: ""
-//     });
+const currentDate = new Date();
+const prevDay = new Date(
+  currentDate.getFullYear(),
+  currentDate.getMonth(),
+  currentDate.getDate() - 1,
+);
 
-//     console.log("response", response);
-//   },
-//   null,
-//   true,
-//   'America/Los_Angeles'
-// );
+const getMessageDataFromDB = async (db: NodePgDatabase<typeof schema>) => {
+  return db.select({
+    message_id: schema.messageDashboard.messageId,
+    created_at: schema.messageDashboard.createdAt,
+    chat_session_id: schema.messageDashboard.chatSessionId,
+    organization_id: schema.messageDashboard.organizationId,
+    project_id: schema.messageDashboard.projectId,
+    question: schema.messageDashboard.question,
+    thumbs_up: schema.messageDashboard.thumbsUp,
+    thumbs_down: schema.messageDashboard.thumbsDown,
+    is_feature_supported: schema.messageDashboard.isFeatureSupported,
+  }).from(schema.messageDashboard).where(and(gte(schema.messageDashboard.createdAt, prevDay), lte(schema.messageDashboard.createdAt, currentDate)))
+}
 
-// job.start();
+const sendMessageToSlackChannel = async ({
+  countResponseQuestions,
+  countQuestionsWithoutAnswers,
+  countThumbsUp,
+  countThumbsDown
+}: Record<string, number>) => {
+  const { message } = await app.client.chat.postMessage({
+    channel: process.env.NOTIFICATION_CHANNEL_ID!,
+    parse: 'none',
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: "mrkdwn",
+          text: `
+*Inkeep Report (${prevDay.toLocaleString('default', { month: 'long' })} ${prevDay.getDate()}, ${prevDay.getFullYear()})*\n
+• Questions answered: ${countResponseQuestions} :speech_balloon: \n
+• Ratings: ${countThumbsUp} :+1: / ${countThumbsDown} :-1: \n
+• # of questions without answers in content: ${countQuestionsWithoutAnswers}. See in full :point_down:
+            `
+        },
+      },
+    ]
+  });
+
+  return message;
+}
+
+const formatterListQuestionsWithoutAnswers = (questionsWithoutAnswers: GetQuestionFromMessageDashboardTable) => {
+  let message = ""
+
+  questionsWithoutAnswers.forEach(({ question, organization_id, chat_session_id, project_id }) => {
+    const questionFormatter = question && question.length > 200 ? `${question.substring(0, 200)}...` : question;
+
+    let formatedText = `
+*Question: ${questionFormatter}* \n
+<${process.env.INKEEP_PORTAL_URL || "https://portal.inkeep.com"}/${organization_id}/projects/${project_id}/chat-sessions?chatId=${chat_session_id}|See in full>`
+
+    message += formatedText
+  })
+
+  return message
+}
 
 const main = async () => {
   try {
-    const currentDate = new Date("2024-05-15 18:46:55.423 +0300");
-    const prevDay = new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth(),
-      currentDate.getDate() - 1,
-    );
 
     const db = drizzle(drizzleClient, { schema, logger: true });
     await initializationDrizzleClient(drizzleClient, db);
@@ -62,21 +110,51 @@ const main = async () => {
     await app.start(process.env.PORT || 3000);
     console.log('⚡️ Bolt app is running! ⚡️');
 
-    const repsonseQuestions = await db.select({
-      message_id: schema.messageDashboard.messageId,
-      created_at: schema.messageDashboard.createdAt,
-      chat_session_id: schema.messageDashboard.chatSessionId,
-      question: schema.messageDashboard.question,
-      thumbs_up: schema.messageDashboard.thumbsUp,
-      thumbs_down: schema.messageDashboard.thumbsDown,
-      is_feature_supported: schema.messageDashboard.isFeatureSupported,
-    }).from(schema.messageDashboard).where(and(gte(schema.messageDashboard.createdAt, prevDay), lte(schema.messageDashboard.createdAt, currentDate)))
+    new CronJob(
+      '* */24 * * *', // cronTime (every 24 hours)
+      async function () {
+        console.log("cron job run")
 
-    console.log("repsonseQuestions", repsonseQuestions);
+        let countThumbsUp = 0;
+        let countThumbsDown = 0;
+
+        const responseQuestions = await getMessageDataFromDB(db)
+
+        const questionsWithoutAnswers = responseQuestions.filter(responseQuestion => {
+          if (responseQuestion.thumbs_up) ++countThumbsUp;
+          if (responseQuestion.thumbs_down) ++countThumbsDown;
+
+          return !responseQuestion.is_feature_supported
+        });
+
+        const responseSendMessage = await sendMessageToSlackChannel({
+          countResponseQuestions: responseQuestions.length,
+          countQuestionsWithoutAnswers: questionsWithoutAnswers.length,
+          countThumbsUp,
+          countThumbsDown
+        })
+
+        await app.client.chat.postMessage({
+          channel: process.env.NOTIFICATION_CHANNEL_ID!,
+          thread_ts: responseSendMessage?.ts,
+          blocks: [
+            {
+              type: 'section',
+              text: {
+                type: "mrkdwn",
+                text: formatterListQuestionsWithoutAnswers(questionsWithoutAnswers),
+              },
+            },
+          ]
+        })
+
+      },
+      null,
+      true,
+    ).start();
+
   } catch (error) {
     console.error('Unable to start App', error);
-  } finally {
-    await drizzleClient.end()
   }
 }
 
